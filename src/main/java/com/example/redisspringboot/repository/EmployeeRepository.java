7ee89e75b55de16ef3e01f9403d75b941992cf84
longs7 <longkeyvic1@gmail.com>
2021-11-08 11:21:10 +0700

    crud list

--- src/main/java/com/example/redisspringboot/repository/EmployeeRepository.java
+++ src/main/java/com/example/redisspringboot/repository/EmployeeRepository.java
@@ -12,36 +12,46 @@ import java.util.List;
 public class EmployeeRepository {
 
     private HashOperations hashOperations;//crud hash
-
+    private ListOperations listOperations;
 
 
 
     private RedisTemplate redisTemplate;
 
     public EmployeeRepository(RedisTemplate redisTemplate) {
-
+        this.listOperations = redisTemplate.opsForList();
         this.hashOperations = redisTemplate.opsForHash();
         this.redisTemplate = redisTemplate;
 
     }
 
     public void saveEmployee(Employee employee){
-
-        hashOperations.put("EMPLOYEE", employee.getId(), employee);
+//        hashOperations.put("EMPLOYEE", employee.getId(), employee);
+        listOperations.rightPush("EMPLOYEE_LIST", employee);
     }
     public List<Employee> findAll(){
+//        return hashOperations.values("EMPLOYEE");
 
-        return hashOperations.values("EMPLOYEE");
+        return listOperations.range("EMPLOYEE_LIST", 0, -1);
+
     }
     public Employee findById(Integer id){
+//        return (Employee) hashOperations.get("EMPLOYEE", id);
+        List<Employee> list = findAll();
+        for(Employee e : list) {
+            if(e.getId() == id)
+                return e;
+        };
 
-        return (Employee) hashOperations.get("EMPLOYEE", id);
+        return null;
     }
 
     public void update(Employee employee){
         saveEmployee(employee);
     }
     public void delete(Integer id){
-        hashOperations.delete("EMPLOYEE", id);
+//        hashOperations.delete("EMPLOYEE", id);
+
+        listOperations.remove("EMPLOYEE_LIST", 1, findById(id));
     }
 }
